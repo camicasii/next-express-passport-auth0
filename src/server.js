a6b08ff674fb2b848f2b7da66017f9ed7cc3aa31
Jhonattan ramirez <camicasii@hotmail.com>
2019-12-08 17:44:14 -0400

    develop

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 import next from "next";
 import http from "http";
-import server from "./server.conf";
+//import server from "./server.conf";
+import express from 'express';
 
 const dev = process.env.NODE_ENV !== "production";
 
@@ -13,6 +14,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   // handling everything else with Next.js
+  const server = express();
   server.get("*", handle);
   http
     .createServer(server)
